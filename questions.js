7ef6cf4db7f478755f0905c9b2b817cca4ecdcf8
AustinSFleet <austinsfleet@gmail.com>
2017-09-10 12:16:15 -0500

    added everything

---
+++ questions.js
@@ -0,0 +1,14 @@
+exports.questions = [
+	{
+		full: 'Sherlock Holmes lived at 221B Baker St.',
+		cloze: 'Sherlock'
+	},
+	{
+		full: 'Ali Baba spoke Open Sesame to enter the Cave of Wonders',
+		cloze: 'Sesame'
+	},
+	{
+		full: 'Jason led the Argonauts on an expedition for the Golden Fleece',
+		cloze: 'Jason'
+	}
+]
